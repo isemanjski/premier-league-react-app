eb0205bc8c7bb2c51eb01341cf72baf41fe548ae
Ivan Šemanjski <ivan.semanjski@gmail.com>
2017-11-03 00:45:53 +0100

    Fixed some UI layout issues

--- src/components/team/TeamHeader.spec.tsx
+++ src/components/team/TeamHeader.spec.tsx
@@ -2,17 +2,10 @@ import * as React from 'react';
 import * as enzyme from 'enzyme';
 import { TeamHeader } from './TeamHeader';
 import { Team } from '../../api/models';
-import { Emblem } from '../_shared/Emblem';
 
 const team1: Team = new Team({ id: 'team-1', name: 'Team 1' });
 
 it('renders the correct team name', () => {
   const component = enzyme.shallow(<TeamHeader team={team1}/>);
   expect(component.find('.team-name').text()).toEqual('Team 1');
-});
-
-it('renders the Emblem child component', () => {
-  const component = enzyme.mount(<TeamHeader team={team1}/>);
-  expect(component.find('<Emblem>')).toEqual('Hello Daniel!');
-  expect(component.contains(<Emblem team={team1}/>));
 });
